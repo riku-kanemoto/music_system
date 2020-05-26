2d109ba65e808aaf3c5dc2334e78150ce1c22737
riku.kanemoto <sky.riku1225@gmail.com>
2020-05-26 22:37:19 +0900

    Add password encryption

---
+++ src/controllers/member/MemberCreateServlet.java
@@ -0,0 +1,41 @@
+package controllers.member;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class MemberCreateServlet
+ */
+@WebServlet("/member/create")
+public class MemberCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        String _token=(String)request.getParameter("_token");
+        if(_token !=null &&_token.equals(request.getSession().getId())){
+            EntityManager em=DBUtil.createEntityManager();
+
+        }
+    }
+
+}
